c34f9f7753fe17516d7b92252542be49512e3276
ay0530 <edkdud530@gmail.com>
2023-12-06 10:28:36 +0900

    ♻️ refactor : 변수명 일괄 변경, sequelize -> prisma, 토큰 오류 수정

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,9 +1,7 @@
-const jwt = require("jsonwebtoken");
-const { Op } = require('sequelize');
-const { Members } = require("../models/index");
-
 require('dotenv').config();
-const env = process.env;
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require('@prisma/client'); // prisma 패키지
+const prisma = new PrismaClient();
 
 // 사용자 인증 미들웨어
 module.exports = async (req, res, next) => {
@@ -16,27 +14,24 @@ module.exports = async (req, res, next) => {
     if (!authToken || authType !== "Bearer") { throw new Error("401-로그인전"); }
 
     // 조회 : 회원 정보
-    const { m_id } = jwt.verify(authToken, env.PRIVATE_KEY, (err, decoded) => {
-      if (err) {
-        console.log(err + "!");
-        // ERR 403 : 토큰 유효기간 만료
-        if (err.name === 'TokenExpiredError') { throw new Error("403-토큰유효기간만료"); }
-      }
-    });
-    const member = await Members.findOne({
+    const decoded = await jwt.verify(authToken, process.env.PRIVATE_KEY);
+    const { login_id } = decoded;
+
+    const user = await prisma.USER.findMany({
       where: {
-        [Op.or]: [
-          { m_id: m_id },
-          { m_email: m_id }
+        OR: [
+          { login_id: { contains: login_id, } },
+          { email: { contains: login_id, } }
         ]
       }
     });
 
-    res.locals.member = member;    // localStorage에 member 값 저장
+    res.locals.user = user[0];    // localStorage에 user 값 저장
     next();
   } catch (err) {
-    console.log(err);
-    if (err.message === "401-로그인전") {
+    if (err.name === 'TokenExpiredError') {
+      throw new Error("403-토큰유효기간만료");
+    } else if (err.message === "401-로그인전") {
       res.status(401).send({ errorMessage: "로그인 후 이용 가능한 기능입니다." });
     } else if (err.message = "403-토큰유효기간만료") {
       return res.status(403).json({ message: '토큰이 만료되었습니다.' });
