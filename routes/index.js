bcc65ffdb7782207475b1399e4af8dd695fe2abb
ay0530 <edkdud530@gmail.com>
2023-12-06 10:29:29 +0900

    ♻️ refactor : 애플리케이션 초기세팅 sequelize 제거

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const sequelize = require('../models').sequelize;
-
-// db연결
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 const membersRouter = require('./members');
 const productsRouter = require('./products');
