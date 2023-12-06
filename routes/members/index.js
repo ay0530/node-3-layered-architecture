9a41cfb572d3e151f1819c4a313941f2a4b4f52a
ay0530 <edkdud530@gmail.com>
2023-12-06 10:29:10 +0900

    refactor : sequelize -> prisma

--- routes/members/index.js
+++ routes/members/index.js
@@ -11,17 +11,17 @@ const authMiddleware = require('../../middlewares/auth-middleware.js'); // auth-
 
 // 회원 정보 저장(CREATE)
 router.post('/signup', async (req, res) => {
-  const { username, password, confirmPassword, name, email } = req.body;
+  const { login_id, password, confirmPassword, name, email } = req.body;
   try {
     // ERR 400 : 아이디 중복
-    const existsUsername = await prisma.USER.findUnique({
-      where: { username }
+    const existslogin_id = await prisma.USER.findUnique({
+      where: { login_id }
     });
-    if (existsUsername) { throw new Error("400-아이디중복"); }
+    if (existslogin_id) { throw new Error("400-아이디중복"); }
 
     // ERR 400 : 이메일 중복
-    const existsEmail = await prisma.USER.findOne({
-      where: { username: username }
+    const existsEmail = await prisma.USER.findUnique({
+      where: { email }
     });
     if (existsEmail) { throw new Error("400-이메일중복"); }
 
@@ -35,17 +35,19 @@ router.post('/signup', async (req, res) => {
     // await bcrypt.hash(비밀번호, 길이); : 비밀번호를 암호화
     const newPassword = await bcrypt.hash(password, 10);
     // 저장 : 회원정보
-    const member = await prisma.USER.create({ username, password: newPassword, name, email });
-    await member.save();
+    const user = await prisma.USER.create({
+      data: {
+        login_id,
+        password: newPassword,
+        name,
+        email,
+      },
+    });
+    await prisma.$disconnect(); // prisma 연결 끊기
 
-    res.status(201).json({ memberInfo: { username, name, email } });
+    res.status(201).json({ userInfo: { login_id, name, email } });
   } catch (error) {
-    // SequelizeValidationError : Models의 유효성 검사 에러
-    if (error.name === 'SequelizeValidationError') {
-      const validationErrors = error.errors.map(err => err.message.replace('Validation error: ', ''));
-      return res.status(400).json({ errorMessage: validationErrors });
-    }
-    else if (error.message === "400-아이디중복") {
+    if (error.message === "400-아이디중복") {
       return res.status(400).json({ errorMessage: "이미 등록된 아이디입니다." });
     }
     else if (error.message === "400-이메일중복") {
@@ -56,6 +58,8 @@ router.post('/signup', async (req, res) => {
     }
     else if (error.message === "400-비밀번호길이") {
       return res.status(400).json({ errorMessage: "비밀번호는 6자 이상 입력해주세요." });
+    } else {
+      console.log(error);
     }
   }
 });
@@ -63,24 +67,23 @@ router.post('/signup', async (req, res) => {
 // 로그인 및 인증정보 생성CREATE)
 router.post("/login", async (req, res) => {
   try {
-    const { username, password } = req.body; // body 값 조회
+    const { login_id, password } = req.body; // body 값 조회
 
     // 조회 : 회원 정보
-    const member = await prisma.USER.findOne({
+    const user = await prisma.USER.findMany({
       where: {
-        // [Op.or] : where ... or ...()
-        [Op.or]: [
-          { username: username },
-          { email: username }
+        OR: [
+          { login_id: { contains: login_id, } },
+          { email: { contains: login_id, } }
         ]
       }
     });
 
     // ERR 400 : 아이디, 이메일 미존재
-    if (!member) { throw new Error("400-아이디미존재"); }
+    if (!user) { throw new Error("400-아이디미존재"); }
 
     // 조회 : 암호화된 비밀번호
-    const passwordValue = member.get("password");
+    const passwordValue = user[0].password;
     // bcrypt.compare(사용자가 로그인 시 입력한 비밀번호, DB에 저장된 암호화 비밀번호) : 
     const equalPassword = await bcrypt.compare(password, passwordValue);
 
@@ -89,7 +92,7 @@ router.post("/login", async (req, res) => {
 
     // 로그인 성공 시 토큰 생성
     // jwt.sign({payload},"암호키",{expiresIn: 유효 시간}) : 토큰 생성
-    const token = await jwt.sign({ username }, process.env.PRIVATE_KEY, { expiresIn: "12h" });
+    const token = await jwt.sign({ login_id }, process.env.PRIVATE_KEY, { expiresIn: "12h" });
     res.cookie("Authorization", `Bearer ${token}`); // res.cookie("Authorization", `Bearer ${token}`) : 쿠키에 토큰 저장
     res.status(200).json({ token: token });
   } catch (error) {
@@ -98,6 +101,8 @@ router.post("/login", async (req, res) => {
     }
     else if (error.message === "400-비밀번호불일치") {
       res.status(400).json({ errorMessage: "비밀번호가 일치하지 않습니다." });
+    } else {
+      console.log(error);
     }
   }
 });
@@ -105,8 +110,8 @@ router.post("/login", async (req, res) => {
 // 내 정보 조회
 router.get("/me", authMiddleware, async (req, res) => {
   // res.locals.@@ : localstorage에 저장된 정보 조회
-  const { username, email, name } = res.locals.member;
-  res.status(200).json({ myInfo: { username, name, email } });
+  const { login_id, email, name } = res.locals.user;
+  res.status(200).json({ myInfo: { login_id, name, email } });
 });
 
 module.exports = router;
