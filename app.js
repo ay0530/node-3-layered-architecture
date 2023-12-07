bd460f954acbfeb8e6531bea46112cb89c2c410a
ay0530 <edkdud530@gmail.com>
2023-12-07 18:53:46 +0900

    ✨ update : 에러 핸들러 추가

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express'); // express 라이브러리를 변수에 할당
+const routes = require('./routes');
 const app = express(); // express를 실행해서 app 객체 생성
 const port = 5608; // 서버 포트
 const cookieParser = require('cookie-parser');
+const { CustomErrorHandler } = require('./error-handlers/custom.error.handlers');
+const { ValidationErrorHandler } = require('./error-handlers/validation.error.handler');
 
 app.use(express.json());
 app.use(cookieParser());
 
-const routes = require('./routes');
-
 app.use("/", routes);
+app.use(ValidationErrorHandler);
+app.use(CustomErrorHandler);
 
 // listen 메서드로 서버 실행
 app.listen(port, () => {
