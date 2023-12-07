a49f217f79b1f6768ccd366ac1b97641c05cb0d5
ay0530 <edkdud530@gmail.com>
2023-12-07 18:52:49 +0900

    ✨ update : 에러 핸들러 추가

--- routes/members/index.js
+++ routes/members/index.js
@@ -5,37 +5,49 @@ const { PrismaClient } = require('@prisma/client'); // prisma 패키지
 const prisma = new PrismaClient();
 const jwt = require('jsonwebtoken'); // jwt 패키지 - 인증
 const bcrypt = require('bcrypt'); // bcrypt 패키지 - 비밀번호 해시
-const authMiddleware = require('../../middlewares/auth-middleware.js'); // auth-middleware.js 조회
-
-
+const authMiddleware = require('../../middlewares/auth.middleware.js'); // auth.middleware.js 조회
+// 에러처리
+const { CustomError, ErrorTypes, UserValidError, } = require('../../error-handlers/custom.errors.js');
+const { validationResult } = require('express-validator');
+const { userSignupValidate } = require('../../validator/user.validator.js');
+const { userLoginIdValidate } = require('../../validator/user.validator.js');
 
 // 회원 정보 저장(CREATE)
-router.post('/signup', async (req, res) => {
+router.post('/signup', userSignupValidate, async (req, res, next) => {
+  const errors = validationResult(req);
   const { login_id, password, confirmPassword, name, email } = req.body;
   try {
+    // 입력하지 않은 값이 있을 경우;
+    if (!errors.isEmpty()) {
+      throw new UserValidError();
+    }
+
     // ERR 400 : 아이디 중복
-    const existslogin_id = await prisma.USER.findUnique({
+    const existsLoginId = await prisma.USER.findUnique({
       where: { login_id }
     });
-    if (existslogin_id) { throw new Error("400-아이디중복"); }
+    if (existsLoginId) {
+      throw new CustomError(ErrorTypes.UserLoginIdExistError);
+    }
 
     // ERR 400 : 이메일 중복
     const existsEmail = await prisma.USER.findUnique({
       where: { email }
     });
-    if (existsEmail) { throw new Error("400-이메일중복"); }
+    if (existsEmail) {
+      throw new CustomError(ErrorTypes.UserEmailExistError);
+    }
 
-    // ERR 400 : 비밀번호 불일치
-    if (password !== confirmPassword) { throw new Error("400-비밀번호불일치"); }
-
-    // ERR 404 : 비밀번호 최소 길이 불충족 
-    if (password.length < 6) { throw new Error("400-비밀번호길이"); };
+    // 비밀번호 불일치
+    if (password !== confirmPassword) {
+      throw new CustomError(ErrorTypes.UserConfirmPwMismatchError);
+    }
 
     // 저장 : 비밀번호 암호화
     // await bcrypt.hash(비밀번호, 길이); : 비밀번호를 암호화
     const newPassword = await bcrypt.hash(password, 10);
     // 저장 : 회원정보
-    const user = await prisma.USER.create({
+    await prisma.USER.create({
       data: {
         login_id,
         password: newPassword,
@@ -47,25 +59,13 @@ router.post('/signup', async (req, res) => {
 
     res.status(201).json({ userInfo: { login_id, name, email } });
   } catch (error) {
-    if (error.message === "400-아이디중복") {
-      return res.status(400).json({ errorMessage: "이미 등록된 아이디입니다." });
-    }
-    else if (error.message === "400-이메일중복") {
-      return res.status(400).json({ errorMessage: "이미 등록된 이메일입니다." });
-    }
-    else if (error.message === "400-비밀번호불일치") {
-      return res.status(400).json({ errorMessage: "비밀번호와 비밀번호 확인에 입력한 값이 일치하지 않습니다." });
-    }
-    else if (error.message === "400-비밀번호길이") {
-      return res.status(400).json({ errorMessage: "비밀번호는 6자 이상 입력해주세요." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
   }
 });
 
 // 로그인 및 인증정보 생성CREATE)
-router.post("/login", async (req, res) => {
+router.post("/login", userLoginIdValidate, async (req, res, next) => {
   try {
     const { login_id, password } = req.body; // body 값 조회
 
@@ -80,38 +80,41 @@ router.post("/login", async (req, res) => {
     });
 
     // ERR 400 : 아이디, 이메일 미존재
-    if (!user) { throw new Error("400-아이디미존재"); }
+    if (!user) {
+      throw new CustomError(ErrorTypes.UserloginIdNotExistError);
+    }
 
-    // 조회 : 암호화된 비밀번호
+    // 비밀번호 암호화
     const passwordValue = user[0].password;
-    // bcrypt.compare(사용자가 로그인 시 입력한 비밀번호, DB에 저장된 암호화 비밀번호) : 
     const equalPassword = await bcrypt.compare(password, passwordValue);
 
     // ERR 404 : 비밀번호 불일치
-    if (!equalPassword) { throw new Error("400-비밀번호불일치"); }
+    if (!equalPassword) {
+      throw new CustomError(ErrorTypes.UserPasswordMismatchError);
+    }
 
     // 로그인 성공 시 토큰 생성
     // jwt.sign({payload},"암호키",{expiresIn: 유효 시간}) : 토큰 생성
-    const token = await jwt.sign({ login_id }, process.env.PRIVATE_KEY, { expiresIn: "12h" });
+    const token = await jwt.sign({ login_id }, "lay-secret-key", { expiresIn: "12h" });
     res.cookie("Authorization", `Bearer ${token}`); // res.cookie("Authorization", `Bearer ${token}`) : 쿠키에 토큰 저장
     res.status(200).json({ token: token });
   } catch (error) {
-    if (error.message === "400-아이디미존재") {
-      res.status(400).json({ errorMessage: "아이디 또는 이메일이 존재하지 않습니다." });
-    }
-    else if (error.message === "400-비밀번호불일치") {
-      res.status(400).json({ errorMessage: "비밀번호가 일치하지 않습니다." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
+    console.log(error);
   }
 });
 
 // 내 정보 조회
-router.get("/me", authMiddleware, async (req, res) => {
-  // res.locals.@@ : localstorage에 저장된 정보 조회
+router.get("/me", authMiddleware, async (req, res, next) => {
   const { login_id, email, name } = res.locals.user;
-  res.status(200).json({ myInfo: { login_id, name, email } });
+  try {
+    res.status(200).json({ myInfo: { login_id, name, email } });
+  } catch (error) {
+    console.log(error);
+    next(error);
+    console.log(error);
+  }
 });
 
 module.exports = router;
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,24 +1,30 @@
 const express = require("express"); // express 받아오기
 const router = express.Router();
 const jwt = require('jsonwebtoken'); // jwt 패키지 - 인증
-const authMiddleware = require('../../middlewares/auth-middleware.js'); // auth-middleware.js 조회
+const authMiddleware = require('../../middlewares/auth.middleware.js'); // auth.middleware.js 조회
 const { PrismaClient } = require('@prisma/client'); // prisma 패키지
 const prisma = new PrismaClient();
-
+// 에러처리
+const { CustomError, ErrorTypes, ProductValidError, } = require('../../error-handlers/custom.errors.js');
+const { validationResult } = require('express-validator');
+const { productValidate } = require('../../validator/product.validator.js'); // auth.middleware.js 조회
 // // 상품 정보 저장
-router.post("/", authMiddleware, async (req, res) => {
+router.post("/", authMiddleware, productValidate, async (req, res, next) => {
+  const errors = validationResult(req);
   try {
     const { name, description } = req.body;    // body 값 조회
     const { login_id } = res.locals.user;
+
+    // ERR 400 : 데이터가 하나라도 입력되지 않은 경우
+    if (!errors.isEmpty()) {
+      throw new ProductValidError();
+    }
 
     // 조회 : 회원 번호
     const userId = await prisma.USER.findUnique({
       select: { id: true },
       where: { login_id }
     });
-
-    // ERR 400 : 데이터가 하나라도 입력되지 않은 경우
-    if (!name || !description) { throw new Error("400-데이터입력err"); }
 
     // 저장 : 상품정보
     const product = await prisma.PRODUCT.create({
@@ -31,74 +37,65 @@ router.post("/", authMiddleware, async (req, res) => {
     await prisma.$disconnect(); // prisma 연결 끊기
     res.status(201).json({ message: "판매 상품을 등록하였습니다." });
   } catch (error) {
-    if (error.message === "400-데이터입력err") {
-      return res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
   }
 });
 
 // //  상품 정보 수정
-router.put("/:id", authMiddleware, async (req, res) => {
+router.put("/:id", authMiddleware, productValidate, async (req, res, next) => {
+  const errors = validationResult(req);
   try {
     const { id } = req.params; // params 값 조회
     const { name, description, status } = req.body; // body 값 조회
     const { login_id } = res.locals.user;
 
+    // ERR 400 : 데이터가 하나라도 입력되지 않은 경우
+    if (!errors.isEmpty()) {
+      throw new ProductValidError();
+    }
     // 조회 : 회원 번호 
     const user = await prisma.USER.findUnique({
       where: { login_id }
     });
     const userId = user.id;
 
-    // ERR 404 : DB에 해당 상품의 Id 값이 존재하지 않은 경우
+    // 조회 : 상품 정보
     const existsProduct = await prisma.PRODUCT.findUnique({
       where: {
         id: +id
       }
     });
-    if (!existsProduct) { throw new Error("404-상품미저장err"); }
-
-    // ERR 400 : 데이터가 하나라도 입력되지 않은 경우
-    if (!name || !description || !status) { throw new Error("400-데이터입력err"); }
+    // ERR 404 : DB에 해당 상품의 Id 값이 존재하지 않은 경우
+    if (!existsProduct) {
+      throw new CustomError(ErrorTypes.ProductDoesNotExistError);
+    }
     // ERR 403 : 상품을 등록한 계정이 아닌 경우
-    if (userId !== existsProduct.user_id) { throw new Error("403-권한미존재"); }
+    if (userId !== existsProduct.user_id) {
+      throw new CustomError(ErrorTypes.TokenUserDoesNotExistError);
+    }
 
     // 수정 : 상품 정보
     await prisma.PRODUCT.update({
-      where: {
-        id: +id
-      },
       data: {
         name,
         description,
         status,
         updated_at: new Date()
+      }, where: {
+        id: +id
       }
     });
     await prisma.$disconnect(); // prisma 연결 끊기
     res.status(200).json({ message: "상품 정보를 수정하였습니다." });
   } catch (error) {
-    // SequelizeValidationError : Models의 유효성 검사 에러
-    if (error.name === 'SequelizeValidationError') {
-      const validationErrors = error.errors.map(err => err.message.replace('Validation error: ', ''));
-      return res.status(400).json({ errorMessage: validationErrors });
-    }
-    else if (error.message === "400-데이터입력err") {
-      res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
-    } else if (error.message === "403-권한미존재") {
-      res.status(403).json({ errorMessage: "수정 권한이 없습니다." });
-    } else if (error.message === "404-상품미저장err") {
-      res.status(404).json({ errorMessage: "상품 조회에 실패하였습니다." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
   }
 });
 
 // //  상품 정보 삭제
-router.delete("/:id", authMiddleware, async (req, res) => {
+router.delete("/:id", authMiddleware, productValidate, async (req, res, next) => {
   try {
     const { id } = req.params; // params 값 조회
     const { login_id } = res.locals.user;
@@ -116,7 +113,9 @@ router.delete("/:id", authMiddleware, async (req, res) => {
     if (!existsProduct) { throw new Error("404-상품미저장err"); }
 
     // ERR 403 : 상품을 등록한 계정이 아닌 경우
-    if (userId !== existsProduct.user_id) { throw new Error("403-권한미존재"); }
+    if (userId !== existsProduct.user_id) {
+      throw new CustomError(ErrorTypes.TokenUserDoesNotExistError);
+    }
 
     // 삭제 : 상품 정보
     await prisma.PRODUCT.delete({
@@ -125,18 +124,14 @@ router.delete("/:id", authMiddleware, async (req, res) => {
     await prisma.$disconnect(); // prisma 연결 끊기
     res.status(200).json({ message: "상품을 삭제하였습니다." });
   } catch (error) {
-    if (error.message === "403-권한미존재") {
-      res.status(403).json({ errorMessage: "권한이 없습니다." });
-    } else if (error.message === "404-상품미저장err") {
-      res.status(404).json({ errorMessage: "상품 조회에 실패하였습니다." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
   }
 });
 
 // //  상품 정보 전체 조회
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
+  const errors = validationResult(req);
   const { category, order } = req.query; // req 조회
   const orderByField = {}; // 동적 정렬 필드를 담을 빈 객체 생성
   orderByField[category] = order === 'desc' ? 'desc' : 'asc'; // 동적으로 정렬 필드를 설정
@@ -159,7 +154,8 @@ router.get("/", async (req, res) => {
 });
 
 // //  상품 상세 조회
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
+  const errors = validationResult(req);
   try {
     const { id } = req.params; // params 값 조회
     // 조회 : 상품 상세
@@ -183,11 +179,8 @@ router.get("/:id", async (req, res) => {
     await prisma.$disconnect(); // prisma 연결 끊기
     res.status(200).json({ product: product });
   } catch (error) {
-    if (error.message === "404-상품미저장err") {
-      return res.status(404).json({ message: "상품 조회에 실패하였습니다." });
-    } else {
-      console.log(error);
-    }
+    console.log(error);
+    next(error);
   }
 });
 
