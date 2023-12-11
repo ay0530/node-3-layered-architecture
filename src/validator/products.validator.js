1c337d1eaaac724b0c5756baf06805d63b989175
ay0530 <edkdud530@gmail.com>
2023-12-11 10:35:51 +0900

    🐛 bugfix : status 유효성 검사 오류 수정

--- src/validator/products.validator.js
+++ src/validator/products.validator.js
@@ -5,8 +5,9 @@ const productValidate = [
   body('description').trim().isLength({ min: 1 }),
   body('status').custom((value, { req }) => {
     if (value !== 'FOR_SALE' && value !== 'SOLD_OUT') {
-      return false;
+      throw new Error('Invalid status value');
     }
+    return true; // 유효성 검사 통과
   }),
 ];
 
